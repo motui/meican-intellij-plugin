816acf4292dbf3f9d4ec04192fa96ba9f35908c3
GitHub Action <action@github.com>
2021-01-16 02:29:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/motui/meicanintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.motui.meicanintellijplugin.services
+
+import com.github.motui.meicanintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
